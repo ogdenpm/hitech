b5cc6d12d15311eab6a3623987c1f07f280f158f
Mark Ogden <mark@Mark-PC>
2024-02-12 19:19:17 +0000

    Added linux/unix build makefiles

---
+++ Linux/hi.mk
@@ -0,0 +1,37 @@
+.PHONY: all install chkversion
+
+SRCDIR=$(subst /Linux,,$(realpath .))
+ROOT:=$(realpath ../..)
+INSTALLDIR = $(ROOT)/Linux/Install
+
+CFLAGS = -O3 -Wall -I$(SRCDIR) -I$(ROOT)/hishared
+VPATH = $(SRCDIR):$(ROOT)/hishared
+
+all:  $(TARGET) | $(INSTALLDIR)
+	cp -p $(TARGET) $(INSTALLDIR)
+
+publish: distclean mkversion
+	$(MAKE)
+
+# check version and force timestamp change so build
+# information is updated
+mkversion:
+	(cd $(SRCDIR); perl $(ROOT)/Scripts/getVersion.pl -W)
+
+# check version and only force build if it has changed
+chkversion:
+	(cd $(SRCDIR); perl $(ROOT)/Scripts/getVersion.pl -w)
+
+_version.o: _version.h _appinfo.h _version.c appinfo.h
+
+$(TARGET): $(OBJS) _version.o
+	gcc -o $@ $^
+
+clean:
+	rm -f *.o 
+
+distclean: clean
+	rm -f $(TARGET)
+
+rebuild: distclean
+	$(MAKE) all
